be18923d0b265e23db3479efa7dcf06980493505
Johan Nicolaisen Brun <johabru@clab16.idi.ntnu.no>
2020-09-17 12:25:07 +0200

    Run

--- Assignment-1/target/debug/deps/gloom_rs-9c2e982f72638b2b.d
+++ Assignment-1/target/debug/deps/gloom_rs-9c2e982f72638b2b.d
@@ -1,6 +1,6 @@
-/home/shomea/j/johabru/dev/gloom-rs/target/debug/deps/gloom_rs-9c2e982f72638b2b: src/main.rs src/shader.rs src/util.rs
+/home/shomea/j/johabru/dev/TDT4195/Assignment-1/target/debug/deps/gloom_rs-9c2e982f72638b2b: src/main.rs src/shader.rs src/util.rs
 
-/home/shomea/j/johabru/dev/gloom-rs/target/debug/deps/gloom_rs-9c2e982f72638b2b.d: src/main.rs src/shader.rs src/util.rs
+/home/shomea/j/johabru/dev/TDT4195/Assignment-1/target/debug/deps/gloom_rs-9c2e982f72638b2b.d: src/main.rs src/shader.rs src/util.rs
 
 src/main.rs:
 src/shader.rs:
